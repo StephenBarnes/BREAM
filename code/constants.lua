dae90f103558a8dfa0553a1eb983e672134fcd4c
Stephen Barnes <stephen6174@gmail.com>
2024-08-17 12:56:56 +0200

    Separate settings for big power poles vs others, for Desolation

--- code/constants.lua
+++ code/constants.lua
@@ -11,13 +11,23 @@ end
 
 Export.safetyCategoryToTypes = { -- Types of entities controlled by the safety settings.
 	["lamps"] = listToSet{"lamp"},
-	["power-poles"] = listToSet{"electric-pole"},
+	["large-power-poles"] = {},
+	["nonlarge-power-poles"] = {},
 	["railways"] = listToSet{"straight-rail", "curved-rail"},
 	["rail-signals"] = listToSet{"rail-signal", "rail-chain-signal"},
 }
-Export.safetyCategoryToSpecial = { -- Entities that belong in these safety categories, but are not in the expected type.
+Export.safetyCategoryToSpecial = { -- Entities that belong in these safety categories, but we're not including the entire type.
 	["lamps"] = {
 		["assembling-machine"] = listToSet{"deadlock-copper-lamp", "copper-aetheric-lamp-straight", "copper-aetheric-lamp-end"},
+	},
+	["large-power-poles"] = {
+		["electric-pole"] = listToSet{"big-electric-pole"},
+	},
+	["nonlarge-power-poles"] = {
+		["electric-pole"] = listToSet {
+			"small-electric-pole", "medium-electric-pole", "substation", -- vanilla
+			"big-wooden-pole", "small-iron-pole", "small-bronze-pole", -- IR3
+		},
 	},
 }
 
